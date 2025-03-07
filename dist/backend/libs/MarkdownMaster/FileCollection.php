dd0bf307de174a52d6980742d1cded54a30cc8cb
Charlie Powell <cdp1337@veraciousnetwork.com>
2025-03-06 19:31:56 -0500

    Recursively scan for files

--- dist/backend/libs/MarkdownMaster/FileCollection.php
+++ dist/backend/libs/MarkdownMaster/FileCollection.php
@@ -41,10 +41,44 @@ class FileCollection {
 		$this->type = $type;
 		$this->url = Config::GetHost() . Config::GetWebPath() . $type . '.html';
 		$pDir = Config::GetRootPath();
-		$files = glob($pDir . '/' . $type . '/**.md');
+		$files = $this->glob_recursive($pDir . '/' . $type, '*.md');
 		foreach ($files as $file) {
 			$this->files[] = new File($file);
 		}
+	}
+
+	/**
+	 * Search recusively for files in a base directory matching a glob pattern.
+	 * The `GLOB_NOCHECK` flag has no effect.
+	 *
+	 * @param string $base Directory to search
+	 * @param string $pattern Glob pattern to match files
+	 * @param int $flags Glob flags from https://www.php.net/manual/function.glob.php
+	 * @return string[] Array of files matching the pattern
+	 */
+	private function glob_recursive(string $base, string $pattern, int $flags = 0) {
+		$flags = $flags & ~GLOB_NOCHECK;
+
+		if (substr($base, -1) !== DIRECTORY_SEPARATOR) {
+			$base .= DIRECTORY_SEPARATOR;
+		}
+
+		$files = glob($base.$pattern, $flags);
+		if (!is_array($files)) {
+			$files = [];
+		}
+
+		$dirs = glob($base.'*', GLOB_ONLYDIR|GLOB_NOSORT|GLOB_MARK);
+		if (!is_array($dirs)) {
+			return $files;
+		}
+
+		foreach ($dirs as $dir) {
+			$dirFiles = $this->glob_recursive($dir, $pattern, $flags);
+			$files = array_merge($files, $dirFiles);
+		}
+
+		return $files;
 	}
 
 	/**
