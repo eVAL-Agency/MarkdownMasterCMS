e987974be48a944dba64200e45be4817cbe2af19
Charlie Powell <cdp1337@veraciousnetwork.com>
2025-04-02 20:30:22 -0400

    Do not update file headers if not in github

--- update_file_headers.sh
+++ update_file_headers.sh
@@ -18,36 +18,38 @@ else
 	COPYRIGHT="$PROJECT_START $COPYRIGHT_OWNER"
 fi
 
+if [ -n "$GITHUB_OUTPUT" ]; then
+	# If running in a git workflow environment, update file headers
+	# Skip this when in development, so as to not overwrite local changes
+	find dist -type f -name '*.php' | grep -v 'vendor/' | while read FILE; do
+		if ! egrep -q '^ \* @version' "$FILE"; then
+			echo "$FILE missing required attribute: @version"
+		else
+			sed -i "s/^ \* @version.*/ \* @version $VERSION/" "$FILE"
+		fi
 
-find dist -type f -name '*.php' | grep -v 'vendor/' | while read FILE; do
-	if ! egrep -q '^ \* @version' "$FILE"; then
-		echo "$FILE missing required attribute: @version"
-	else
-		sed -i "s/^ \* @version.*/ \* @version $VERSION/" "$FILE"
-	fi
+		if ! egrep -q '^ \* @copyright' "$FILE"; then
+			echo "$FILE missing required attribute: @copyright"
+		else
+			sed -i "s/^ \* @copyright.*/ \* @copyright $COPYRIGHT/" "$FILE"
+		fi
 
-	if ! egrep -q '^ \* @copyright' "$FILE"; then
-		echo "$FILE missing required attribute: @copyright"
-	else
-		sed -i "s/^ \* @copyright.*/ \* @copyright $COPYRIGHT/" "$FILE"
-	fi
+		if ! egrep -q '^ \* @license' "$FILE"; then
+			echo "$FILE missing required attribute: @license"
+		else
+			sed -i "s/^ \* @license.*/ \* @license $LICENSE/" "$FILE"
+		fi
 
-	if ! egrep -q '^ \* @license' "$FILE"; then
-		echo "$FILE missing required attribute: @license"
-	else
-		sed -i "s/^ \* @license.*/ \* @license $LICENSE/" "$FILE"
-	fi
+		if ! egrep -q '^ \* @link' "$FILE"; then
+			echo "$FILE missing required attribute: @link"
+		else
+			sed -i "s#^ \* @link.*# \* @link $LINK#" "$FILE"
+		fi
 
-	if ! egrep -q '^ \* @link' "$FILE"; then
-		echo "$FILE missing required attribute: @link"
-	else
-		sed -i "s#^ \* @link.*# \* @link $LINK#" "$FILE"
-	fi
-
-	if ! egrep -q '^ \* @package' "$FILE"; then
-		echo "$FILE missing required attribute: @package"
-	else
-		sed -i "s/^ \* @package.*/ \* @package $PACKAGE/" "$FILE"
-	fi
-done
-
+		if ! egrep -q '^ \* @package' "$FILE"; then
+			echo "$FILE missing required attribute: @package"
+		else
+			sed -i "s/^ \* @package.*/ \* @package $PACKAGE/" "$FILE"
+		fi
+	done
+fi
